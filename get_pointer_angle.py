7f4d4c46746a7325a98904e61f61ac803cf8665b
tangzhenjie <2819786276@qq.com>
2020-06-08 14:38:49 +0800

    update

--- get_pointer_angle.py
+++ get_pointer_angle.py
@@ -1,3 +1,25 @@
 import cv2
-corrected_img_path = "./img_test_corrected/"
-cv2.createLineSegmentDetector(0)
+import numpy as np
+
+#Read gray image
+corrected_img_path = "./img_test_corrected/test1.png"
+corrected_img = cv2.imread(corrected_img_path, 0)
+
+#Create default parametrization LSD
+lsd = cv2.createLineSegmentDetector(0)
+
+#Detect lines in the image
+lines = lsd.detect(corrected_img)[0]
+
+# Find the pointer according to the prior knowledge of the specified table class
+
+
+# calculate the angle, then get the reading number according to the angle->reading num
+
+
+#Draw detected lines in the image
+drawn_img = lsd.drawSegments(corrected_img, lines)
+
+#Show image
+cv2.imshow("LSD", drawn_img)
+cv2.waitKey(0)
