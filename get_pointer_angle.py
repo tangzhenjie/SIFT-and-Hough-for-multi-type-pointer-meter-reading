7c7fd663f3aba11b4a0050b5727cb68a8ef8796a
tangzhenjie <2819786276@qq.com>
2020-06-10 16:55:20 +0800

    first version

--- get_pointer_angle.py
+++ get_pointer_angle.py
@@ -1,25 +1,60 @@
 import cv2
 import numpy as np
+from PIL import Image
 
-#Read gray image
 corrected_img_path = "./img_test_corrected/test1.png"
-corrected_img = cv2.imread(corrected_img_path, 0)
 
-#Create default parametrization LSD
-lsd = cv2.createLineSegmentDetector(0)
+# 读取图像变成灰度并可以图像预处理
+img = cv2.imread(corrected_img_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#Detect lines in the image
-lines = lsd.detect(corrected_img)[0]
+# 图像的边缘检测
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-# Find the pointer according to the prior knowledge of the specified table class
+# 把图像缩小减少运算量
+edges_img = Image.fromarray(edges)
+w, h = edges_img.size
+edges_img_resized = edges_img.resize((w // 3, h // 3))
+edges_img_resized_array = np.array(edges_img_resized)
 
+# 使用Hough Circle Transform对缩小的图像进行仪表盘的检测。
+circles = cv2.HoughCircles(edges_img_resized_array, cv2.HOUGH_GRADIENT, 1, 100,
+                            param1=150, param2=100, minRadius=0, maxRadius=0)
+circles_int = np.uint16(np.around(circles))
+x, y, _ = circles[0][0]  # 假设找到一个最大的圆 ！！！！！！！！
+x, y = x * 3, y * 3   # 缩小前原图仪表盘的圆心
 
-# calculate the angle, then get the reading number according to the angle->reading num
+# 使用Hough Line Transform对原始灰度图进行检测直线指针检测
+lines = cv2.HoughLines(edges, 1, np.pi/180, 125).squeeze(1)
 
+# 使用仪表盘圆心坐标进行正则化选出指针
+pointer_arr = []  # 目前是只用仪表盘圆心坐标就能选出指针来 ！！！！！！！！！
+for rho, theta in lines:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    threshold = np.abs(rho - x * a - y * b)
+    if threshold < 10: #  这个参数得试验得到 ！！！！！！！！！！
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
 
-#Draw detected lines in the image
-drawn_img = lsd.drawSegments(corrected_img, lines)
+        cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
+        pointer_arr.append((rho, theta))
 
-#Show image
-cv2.imshow("LSD", drawn_img)
+# 求出指针角度
+
+for i in circles_int[0, :]:
+    # draw the outer circle
+    cv2.circle(edges_img_resized_array, (i[0], i[1]), i[2],(255,255,0), 2)
+    # draw the center of the circle
+    cv2.circle(edges_img_resized_array, (i[0], i[1]), 2, (255,0,0), 3)
+
+# 显示图像
+cv2.imshow("edges", edges)
+cv2.imshow("img", img)
+cv2.imshow("edges_resized", edges_img_resized_array)
 cv2.waitKey(0)
+
--- test.py
+++ test.py
@@ -1,9 +1,30 @@
-from PIL import Image
-from pylab import *
+import cv2
+import numpy as np
 
-im = array(Image.open('img_test/test3.jpg'))
-imshow(im)
-print('Please click 3 points')
-x = ginput(3)
-print('you clicked:', x)
-#show()
+img = cv2.imread('./template/class1.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+cv2.imshow("LSD1", edges)
+cv2.imshow("LSD2", img)
+cv2.waitKey(0)
+
+
+# 把图像缩小减少运算量
+#edges_img = Image.fromarray(edges)
+#w, h = edges_img.size
+#edges_img_resized = edges_img.resize((w // 3, h // 3))
+#edges_img_resized_array = np.array(edges_img_resized)
